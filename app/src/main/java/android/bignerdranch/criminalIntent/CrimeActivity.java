06bace60731d5664242928e42d3e488948c7b2fd
usc-sy033 <s_y033@student.usc.edu.au>
2019-10-15 17:52:10 +1000

    first commit

--- app/src/main/java/android/bignerdranch/criminalIntent/CrimeActivity.java
+++ app/src/main/java/android/bignerdranch/criminalIntent/CrimeActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
-
+    public static final int REQUEST_ERROR = 0;
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
@@ -44,5 +49,24 @@ public class CrimeActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeFragment();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (errorCode != ConnectionResult.SUCCESS){
+            Dialog errorDialog  = apiAvailability.getErrorDialog(this, errorCode,
+                    REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            finish();
+                        }
+                    });
+            errorDialog.show();
+        }
     }
 }
